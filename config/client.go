0d5a18541bc6eaeca4dd3242d131e2b57b83986c
Abdelmalek El Mellouki <abdelmalek3a@gmail.com>
2022-01-08 06:08:02 +0100

    added redis client

---
+++ config/client.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/joho/godotenv"
+)
+
+func RedisClient(ctx *context.Context) (*redis.Client, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("There was a problem loading .env file")
+	}
+
+	redisHost := os.Getenv("REDIS_SERVER")
+	redisPort := os.Getenv("REDIS_PORT")
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: redisHost + ":" + redisPort,
+		Password: "",	
+		DB:	0,
+	})
+	
+	_, err = rdb.Ping(*ctx).Result()
+
+	return rdb, err
+}
